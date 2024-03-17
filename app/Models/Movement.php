6f5a2463fd6b388d9565fc3fc1c0ed5ce953ff31
Fabio Ventura <foravatenubi@gmail.com>
2024-03-17 17:13:16 +0100

    Add property annotations to Movement model

--- app/Models/Movement.php
+++ app/Models/Movement.php
@@ -6,10 +6,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Orchid\Attachment\Attachable;
 use Orchid\Filters\Filterable;
 use Orchid\Screen\AsSource;
 
+/**
+ * @property ?Carbon $created_at
+ * @property ?Carbon $updated_at
+ * @property Carbon $date
+ * @property float $amount
+ * @property int $id
+ * @property string $description
+ */
 class Movement extends Model
 {
     use AsSource, Attachable, Filterable, HasFactory;
